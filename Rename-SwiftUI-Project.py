82fcf12e977951949aa72ca84e1f0c6b3c750e1d
oiiboii <omer@tactive.xyz>
2023-04-20 14:25:20 -0400

    all files and folders are changing. content of file changing. code optimized and commented

--- Rename-SwiftUI-Project.py
+++ Rename-SwiftUI-Project.py
@@ -1,75 +1,76 @@
 import os
 import shutil
+import mimetypes
 
-# Ask for the path to the Xcode project
-project_path = input("Enter the path to the Xcode project folder: ")
+def copy_and_rename_project():
+    # Prompt the user for the path to the project folder and the new project name
+    old_project_path = input("Enter the path to the project root folder: ")
+    new_project_name = input("Enter the new project name: ")
+    # Construct the new project path by joining the old project's parent folder and the new project name
+    new_project_path = os.path.join(os.path.dirname(old_project_path), new_project_name)
 
-# Ask for the new project name
-new_project_name = input("Enter the new project name: ")
+    # Copy the old project folder to the new project folder
+    shutil.copytree(old_project_path, new_project_path)
 
-# Get the current project name
-current_project_name = os.path.basename(project_path)
+    # Extract the old project name from the old project path
+    old_project_name = os.path.basename(old_project_path)
 
-# Create the new project directory
-new_project_directory = os.path.join(os.path.dirname(project_path), new_project_name)
-shutil.copytree(project_path, new_project_directory)
+    # Define a helper function to format the project name for use in the Swift file
+    def format_name(name):
+        return name.replace(" ", "_").replace("-", "_") + "App"
 
-# Rename the Xcode project file
-old_project_file_path = os.path.join(
-    new_project_directory, current_project_name + ".xcodeproj"
-)
-new_project_file_path = os.path.join(
-    new_project_directory, new_project_name + ".xcodeproj"
-)
-os.rename(old_project_file_path, new_project_file_path)
+    # Format the old and new project names using the helper function
+    old_project_name_formatted = format_name(old_project_name)
+    new_project_name_formatted = format_name(new_project_name)
 
-# Replace project name in .pbxproj file
-pbxproj_path = os.path.join(new_project_file_path, "project.pbxproj")
-with open(pbxproj_path, "r") as f:
-    contents = f.read()
-    updated_contents = contents.replace(current_project_name, new_project_name)
-with open(pbxproj_path, "w") as f:
-    f.write(updated_contents)
+    # Define a helper function to update and rename a given path
+    def update_and_rename_path(path, old, new):
+        if old in path:
+            new_path = path.replace(old, new)
+            os.rename(path, new_path)
+            print(f"Renamed {path} to {new_path}")
+            return new_path
+        return path
 
-# Replace project name in folders and .swift files, and their contents
-for files in os.walk(new_project_directory):
-    for name in files:
-        print(f"Checking file: {name}")
-        if (name == current_project_name.replace(" ", "_").replace("-", "_") + "App.swift"):
-            file_path = os.path.join(root, name)
-            new_file_name = (new_project_name.replace(" ", "_").replace("-", "_") + "App.swift")
-            new_file_path = os.path.join(os.path.dirname(file_path), new_file_name)
-            shutil.copyfile(file_path, new_file_path)
+    # Rename files and folders containing the old project name with the new project name
+    for root, dirs, files in os.walk(new_project_path):
+        for name in files + dirs:
+            path = os.path.join(root, name)
+            update_and_rename_path(path, old_project_name, new_project_name)
+
+    # Update the CFBundleName key in the Info.plist file
+    info_plist_path = os.path.join(new_project_path, new_project_name, "Info.plist")
+    with open(info_plist_path, "r+") as f:
+        contents = f.read()
+        new_contents = contents.replace(old_project_name, new_project_name)
+        f.seek(0)
+        f.write(new_contents)
+        f.truncate()
+    print(f"Updated {info_plist_path}")
+
+    # Add custom MIME type for .plist files
+    mimetypes.add_type('text/xml', '.plist')
+
+    # Update the main Swift file with the new project name and non-Swift text files
+    for root, dirs, files in os.walk(new_project_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            mime_type, _ = mimetypes.guess_type(file_path)
             
-            with open(new_file_path, "r") as f:
-                contents = f.read()
-                updated_contents = contents.replace(current_project_name, new_project_name)
-            with open(new_file_path, "w") as f:
-                f.write(updated_contents)
-            print(f"Copied {file_path} to {new_file_path}")
-            os.remove(file_path)  # Delete the old file
-        elif name.endswith(".swift"):
-            file_path = os.path.join(root, name)
-            with open(file_path, "r") as f:
-                contents = f.read()
-                updated_contents = contents.replace(
-                    current_project_name, new_project_name
-                )
-            with open(file_path, "w") as f:
-                f.write(updated_contents)
-            print(f"Replaced project name in {file_path}")
+            if file_name.endswith(('.swift', '.plist', '.md', '.json', '.xcconfig', '.xcscheme', '.strings', '.h', '.m', '.cpp', '.hpp')):
+                with open(file_path, "r", errors='replace') as f:
+                    lines = f.readlines()
+                with open(file_path, "w", errors='replace') as f:
+                    for line in lines:
+                        line = line.replace(old_project_name_formatted, new_project_name_formatted)
+                        line = line.replace(old_project_name, new_project_name)
+                        f.write(line)
+                print(f"Updated {file_path}")
 
-            # Replace project name in folder name if necessary
-            if current_project_name.replace(" ", "_").replace("-", "_") in file_path:
-                old_folder_name = os.path.dirname(file_path)
-                new_folder_name = old_folder_name.replace(
-                    current_project_name.replace(" ", "_").replace("-", "_"),
-                    new_project_name.replace(" ", "_").replace("-", "_"),
-                )
-                os.rename(old_folder_name, new_folder_name)
-                print(f"Renamed folder {old_folder_name} to {new_folder_name}")
+                if file_name.endswith(".swift"):
+                    new_file_name = file_name.replace(old_project_name_formatted + ".swift", new_project_name_formatted + ".swift")
+                    new_file_path = os.path.join(root, new_file_name)
+                    os.rename(file_path, new_file_path)
+                    print(f"Renamed {file_path} to {new_file_path}")
 
-# Clear Xcode cache
-os.system("rm -rf ~/Library/Developer/Xcode/DerivedData/*")
-
-print("Done!")
+copy_and_rename_project()
