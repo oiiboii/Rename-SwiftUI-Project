474079eb78bb4d6ad729f859921813c0fcedecce
oiiboii <omer@tactive.xyz>
2023-04-20 14:24:07 -0400

    file copy works, but nested folder name change failes

--- Rename-SwiftUI-Project.py
+++ Rename-SwiftUI-Project.py
@@ -1,49 +1,75 @@
 import os
 import shutil
 
-def copy_and_rename_project():
-    # Prompt the user to enter the project folder and name
-    old_project_path = input("Enter the path to the project root folder: ")
-    new_project_name = input("Enter the new project name: ")
+# Ask for the path to the Xcode project
+project_path = input("Enter the path to the Xcode project folder: ")
 
-    # Create a copy of the old project
-    new_project_path = f"{os.path.dirname(old_project_path)}/{new_project_name}"
-    shutil.copytree(old_project_path, new_project_path)
+# Ask for the new project name
+new_project_name = input("Enter the new project name: ")
 
-    # Get the old project name from the old project path
-    old_project_name = os.path.basename(old_project_path)
+# Get the current project name
+current_project_name = os.path.basename(project_path)
 
-    # Loop through all files and folders in the new project
-    for root, dirs, files in os.walk(new_project_path):
-        for name in files + dirs:
-            # Construct the full path to the file or folder
-            path = os.path.join(root, name)
+# Create the new project directory
+new_project_directory = os.path.join(os.path.dirname(project_path), new_project_name)
+shutil.copytree(project_path, new_project_directory)
 
-            # Rename the file or folder if it contains the old project name
-            if old_project_name in path:
-                new_path = path.replace(old_project_name, new_project_name)
-                os.rename(path, new_path)
-                print(f"Renamed {path} to {new_path}")
+# Rename the Xcode project file
+old_project_file_path = os.path.join(
+    new_project_directory, current_project_name + ".xcodeproj"
+)
+new_project_file_path = os.path.join(
+    new_project_directory, new_project_name + ".xcodeproj"
+)
+os.rename(old_project_file_path, new_project_file_path)
 
-    # Update the CFBundleName key in Info.plist
-    info_plist_path = f"{new_project_path}/{new_project_name}/Info.plist"
-    with open(info_plist_path, "r") as f:
-        contents = f.read()
-    new_contents = contents.replace(old_project_name, new_project_name)
-    with open(info_plist_path, "w") as f:
-        f.write(new_contents)
-    print(f"Updated {info_plist_path}")
+# Replace project name in .pbxproj file
+pbxproj_path = os.path.join(new_project_file_path, "project.pbxproj")
+with open(pbxproj_path, "r") as f:
+    contents = f.read()
+    updated_contents = contents.replace(current_project_name, new_project_name)
+with open(pbxproj_path, "w") as f:
+    f.write(updated_contents)
 
-    # Update the @main struct name in [ProjectName]App.swift
-    app_swift_path = f"{new_project_path}/{new_project_name}/{new_project_name}App.swift"
-    with open(app_swift_path, "r") as f:
-        contents = f.read()
-    new_contents = contents.replace(f"@main struct {old_project_name}", f"@main struct {new_project_name}")
-    with open(app_swift_path, "w") as f:
-        f.write(new_contents)
-    print(f"Updated {app_swift_path}")
+# Replace project name in folders and .swift files, and their contents
+for files in os.walk(new_project_directory):
+    for name in files:
+        print(f"Checking file: {name}")
+        if (name == current_project_name.replace(" ", "_").replace("-", "_") + "App.swift"):
+            file_path = os.path.join(root, name)
+            new_file_name = (new_project_name.replace(" ", "_").replace("-", "_") + "App.swift")
+            new_file_path = os.path.join(os.path.dirname(file_path), new_file_name)
+            shutil.copyfile(file_path, new_file_path)
+            
+            with open(new_file_path, "r") as f:
+                contents = f.read()
+                updated_contents = contents.replace(current_project_name, new_project_name)
+            with open(new_file_path, "w") as f:
+                f.write(updated_contents)
+            print(f"Copied {file_path} to {new_file_path}")
+            os.remove(file_path)  # Delete the old file
+        elif name.endswith(".swift"):
+            file_path = os.path.join(root, name)
+            with open(file_path, "r") as f:
+                contents = f.read()
+                updated_contents = contents.replace(
+                    current_project_name, new_project_name
+                )
+            with open(file_path, "w") as f:
+                f.write(updated_contents)
+            print(f"Replaced project name in {file_path}")
 
-    print("Done!")
+            # Replace project name in folder name if necessary
+            if current_project_name.replace(" ", "_").replace("-", "_") in file_path:
+                old_folder_name = os.path.dirname(file_path)
+                new_folder_name = old_folder_name.replace(
+                    current_project_name.replace(" ", "_").replace("-", "_"),
+                    new_project_name.replace(" ", "_").replace("-", "_"),
+                )
+                os.rename(old_folder_name, new_folder_name)
+                print(f"Renamed folder {old_folder_name} to {new_folder_name}")
 
-# Example usage
-copy_and_rename_project()
+# Clear Xcode cache
+os.system("rm -rf ~/Library/Developer/Xcode/DerivedData/*")
+
+print("Done!")
